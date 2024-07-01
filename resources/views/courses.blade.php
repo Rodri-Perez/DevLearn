3f2c9463bd776e8ca8d6ad388bd80c670e878919
Rodri <156032416+Rodri-Perez@users.noreply.github.com>
2024-07-01 01:41:49 -0300

    Actualizar courses.blade.php

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -8,6 +8,72 @@
         @extends('layouts.menu')
     </header>
     <section>
-        
+            <h1 class="article_card_titulo">Todos Nuestros Cursos</h1>
+    <article class="article_cards">
+        <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_piton.png" class="logo_card">
+                <h3 class="card_titulo">Python</h3>
+                <p></p>
+            </figure>
+        </div>
+        <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+        <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_cmas.png" class="logo_card">
+                <h3 class="card_titulo">C++</h3>
+                <p></p>
+            </figure>
+        </div>
+ <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+  <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+ <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+ <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+ <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+ <div class="card">
+            <figure>
+                <img src="/imagen/logos/logo_laravel.png" class="logo_card">
+                <h3 class="card_titulo">Laravel</h3>
+                <p></p>
+            </figure>
+        </div>
+     
+    </article>
     </section>
 @endsection
