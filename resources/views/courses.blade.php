95fea1bd85693c3db1ebd21229dabcd481b6c755
Rodri <156032416+Rodri-Perez@users.noreply.github.com>
2024-07-01 01:37:40 -0300

    Actualizar courses.blade.php

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -1,66 +1,13 @@
-<html>
-  <head>
-    <link rel="stylesheet" href="{{ asset('css/main.css')}}">
-    <title>about</title>
-  </head>
-  <body>
+@extends('layouts.struct')
+
+@section('title','home')
+
+@section('content')
+
     <header>
-      @extends('layouts.menu')
+        @extends('layouts.menu')
     </header>
-    <section class="cabezera">
-    <figure><img class="about_logo " src='/imagen/logo.jpg'></figure><figcaption ><center><h1 class="imagen_text">Aprende programacion en nuestros cursos</h1></center></figcaption>
-    </article>
-</section>
-<section class="container_about">
-  
-       <article>
-        <h1 class="about_title">introduccion</h1>
-<p class="welcome">bienvenido a devlearn aqui podras aprender a programar mediante cursos completos de los lenguajes de programacion<em>, el objetivo inicial de nuestra empresa es implementar un sistema educativo fiable y adecuado para los programadores</p>
-</article>
-<br>
-<article>
-<h1 class="about_title">Sobre la empresa</h1>
-<p><em>Nuestra empresa busca llevar a todos los programadores que sientan que no tienen los suficientes conocimientos</em>,  o sienten que sus conocimientos estan obsoletos y deben renobarse para estar con toda la informacion actual para entrar al nuevo mundo de la programacion el cual se actualida constantemente. </p>
-
-</article>
-<figure>
-  <img class="about_imagen" src="/imagen/empresa.jpg" alt="">
-  <figcaption>esta es la cede de nuestra empresa</figcaption>
-</figure>
-<br>
-<article>
-<h1 class="about_title">Financiamiento</h1>
-<p>Financiamos nuestra empresa mediante las cuotas diarias de los alumnos ademas que contamos con el soporte economico de empresas que nos financian por darles mantenimiento y ofrecerles los mejores estudiantes como empleados.</p>
-</article>
-<figure>
-  <img class="about_imagen" src="/imagen/finanza.jpg" alt="">
-</figure>
-<br>
-<article>
-        <h1 class="about_title">Utilizacion</h1>
-<p>Nuestra aplicaion se utiliza con un sistema de inscripcion de cursos para acceder a los mismos y poder ir emprendiendo<em>, Para poder utilizar la aplicaion se necesita acceso a internet y tener buena intuicion para saber a donde dirigirse</p>
-</article>
-<article>
-<h1 class="about_title">mira nuestros proyectos</h1>
-  <ol>
-    <li>aplicaciones</li>
-    <li>cursos</li>
-  </ol>
- 
-</section>
-<aside class="home_aside">
-
-<figure>
-     <img src="/imagen/logos/logo_c.png" alt="" class="home_logo_c">
-</figure>
-<p class="home_aside_p">
-     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam quibusdam suscipit dolorum error. Adipisci nam, natus accusantium possimus molestiae ex quis aspernatur, cumque, repudiandae aperiam quos necessitatibus! Ad, repellendus quaerat!
-</p>
-
-</aside>
-<footer>
-    <p>©2024 contactanos en @devlearn</p>
-
-  </footer>
-  </body>
-</html>
+    <section>
+        
+    </section>
+@endsection
